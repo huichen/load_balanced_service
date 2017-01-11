985352cd40f3a48fb6d08ae3af65b4b8c22ac2ce
Hui Chen <unmerged@163.com>
2017-01-11 17:32:37 +0800

    使用新的 content 源

--- load_balanced_service.go
+++ load_balanced_service.go
@@ -7,8 +7,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
 	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
 )
 
 var ErrEmptyService = errors.New("empty service")
